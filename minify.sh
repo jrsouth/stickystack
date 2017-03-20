94fb8ebb4ce7531003d2471263dfa7a5e32d32c6
jrsouth <james.south@gmail.com>
2017-03-20 15:58:23 +0000

    Don't include uglifyjs WARN output in minified file

--- minify.sh
+++ minify.sh
@@ -4,4 +4,4 @@
 # Assumes yarn has been run to pull the harmony branch of uglifyJS2.
 # Could check for yarn/npm availability, but no need yet.
 
-yarn run uglifyjs -- stickystack.js -m -c -v --mangle-props > stickystack.min.js
+yarn run uglifyjs -- stickystack.js -m -c -v --mangle-props -o stickystack.min.js
